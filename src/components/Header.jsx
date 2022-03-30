e9888399ea1e60cf3468109e17594a45c2062ef4
Jhan Bayer <jhanbayer@gmail.com>
2022-03-30 15:55:15 -0300

    adding style in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@
 
 function Header() {
   return (
-    <>
-        <h1>Desde el Header</h1>
-    </>
+    <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">Seguimiento Pacientes {""}
+        <span className="text-indigo-600">Veterinaria</span>
+    </h1>  
   )
 }
 
