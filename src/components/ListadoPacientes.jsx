5e2e91ec4cb6649fead85cf09300631aca5edcfb
Jhan Bayer <jhanbayer@gmail.com>
2022-03-31 17:04:35 -0300

    show text when show text when there are no patients

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -4,21 +4,28 @@ const ListadoPacientes = ({ pacientes }) => {
   console.log(pacientes);
   return (
     <div className="md:w-1/2 lg:w-3/5 ">
-      <h2 className="font-black text-center text-3xl">Listado pacientes</h2>
-      <p className="text-center text-lg mb-3">
-        Administra tus Pacientes y {""}
-        <span className="font-bold text-indigo-600">Citas</span>
-      </p>
-      <div className="md:h-screen overflow-y-scroll w-full">
-        {pacientes.map(paciente => {
-          return (
-            <Paciente
-              paciente={paciente}
-              key={paciente.id}
-            />
-          );
-        })}
-      </div>
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-center text-3xl">Listado pacientes</h2>
+          <p className="text-center text-lg mb-3">
+            Administra tus Pacientes y {""}
+            <span className="font-bold text-indigo-600">Citas</span>
+          </p>
+          <div className="md:h-screen overflow-y-scroll w-full">
+            {pacientes.map((paciente) => {
+              return <Paciente paciente={paciente} key={paciente.id} />;
+            })}
+          </div>
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-center text-3xl">No hay pacientes</h2>
+          <p className="text-center text-lg mb-3">
+            Agrega tus Pacientes y {""}
+            <span className="font-bold text-indigo-600">Aparecerán</span>
+          </p>
+        </>
+      )}
     </div>
   );
 };
